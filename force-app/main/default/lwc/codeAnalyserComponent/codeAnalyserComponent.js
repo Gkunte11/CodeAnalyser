af052b61a865920b73181c748c2977426ceb3bdb
Gaurav Kunte <gkunte11.com>
2025-09-07 14:06:31 -0700

    Testing Phase 1 of this project

--- force-app/main/default/lwc/codeAnalyserComponent/codeAnalyserComponent.js
+++ force-app/main/default/lwc/codeAnalyserComponent/codeAnalyserComponent.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import ClaudeServiceCall from '@salesforce/apex/ClaudeServiceCall.explainCode';
 
-export default class CodeAnalyserComponent extends LightningElement {}
+export default class CodeAnalyserComponent extends LightningElement {
+
+    code = '';
+    explanation = ''
+
+    handleInputChange(event){
+        this.code = event.target.value;
+    }
+
+    explainCodeSnippet(){
+        explainCode({codeSnippet: this.code})
+            .then(result => {
+                this.explanation = result;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+}
