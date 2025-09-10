a6a9917be90def02d5ba12cb960775431a3d94c7
Gaurav Kunte <gkunte11.com>
2025-09-09 21:35:09 -0700

    Batch processing incomplete implementation

--- force-app/main/default/lwc/codeAnalyserComponent/codeAnalyserComponent.js
+++ force-app/main/default/lwc/codeAnalyserComponent/codeAnalyserComponent.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 import explainCode from '@salesforce/apex/ClaudeServiceCall.explainCode';
-import getFileContent from '@salesforce/apex/ClaudeFileHandler.getFileContent';
+import fileBatch from '@salesforce/apex/ClaudeFileHandler.getFileContent';
 
 export default class CodeAnalyserComponent extends LightningElement {
 
@@ -9,6 +9,7 @@ export default class CodeAnalyserComponent extends LightningElement {
     lineByLine = false;
     uploadedFileName = '';
     optimizationRequested = false;
+    uploadedFileId = '';
 
     handleInputChange(event){
         this.code = event.target.value;
@@ -30,8 +31,10 @@ export default class CodeAnalyserComponent extends LightningElement {
         if(uploadedfile.length > 0){
 
             const file = uploadedfile[0];
-            const allowedFileExtensions = ['java','py','js','html','css','php','c','cpp','cs','cls'];
+            this.uploadedFileName = file.name;
+            this.uploadedFileId = file.documentId;
             
+            const allowedFileExtensions = ['java','py','js','html','css','php','c','cpp','cs','cls'];
             const ext = file.name.split('.').pop().toLowerCase();
 
             if(!allowedFileExtensions.includes(ext)){
@@ -40,23 +43,65 @@ export default class CodeAnalyserComponent extends LightningElement {
             }
 
 
-            getFileContent({ contentDocumentId: file.documentId })
-                .then(result => { this.code = result; })
-                .catch(error => { console.error(error); });
+            // getFileContent({ contentDocumentId: file.documentId })
+            //     .then(result => { this.code = result; })
+            //     .catch(error => { console.error(error); });
 
-            this.uploadedFileName = file.name;
+            // The above code commented code will be handled in a different class as it will be used for batch processing
 
         }
         
     }
 
     explainCodeSnippet(){
-        explainCode({codeSnippet: this.code , lineByLine: this.lineByLine, optimizationRequested: this.optimizationRequested})
+        // explainCode({codeSnippet: this.code , lineByLine: this.lineByLine, optimizationRequested: this.optimizationRequested})
+        //     .then(result => {
+        //         this.explanation = result;
+        //     })
+        //     .catch(error => {
+        //         console.error(error);
+        //     });
+
+        // Modifying the above action to handle file upload and copy/paste process differently. The File upload will be handled via batch processing as the code files might be bigger in size
+
+        if(this.uploadedFileId){
+
+            fileBatch({
+
+                contentDocumentId: this.uploadedFileId,
+                lineByLine: this.lineByLine,
+                optimizationRequested: this.optimizationRequested
+            })
             .then(result => {
-                this.explanation = result;
+                this.explanation = 'Batch processing started for the uploaded file. The following Code Analysis History record will be updated as soon as the processing has completed: ' + result;
             })
             .catch(error => {
                 console.error(error);
+                this.explanation = 'Error: ' + error;
             });
+
+        }
+
+        else if(this.code){
+
+            explainCode({
+                codeSnippet: this.code, 
+                lineByLine: this.lineByLine, 
+                optimizationRequested: this.optimizationRequested
+            })
+            .then(result => {
+                this.explanation = 'The pasted code will be handled synchronously. Code Analysis History record: ' + result;
+            })
+            .catch(error => {
+                console.error(error);
+                this.explanation = 'Error: ' + error;
+            });
+
+        }
+
+        else{
+
+            this.explaination = 'Please upload a file or paste the code in the text area.';
+        }
     }
 }
